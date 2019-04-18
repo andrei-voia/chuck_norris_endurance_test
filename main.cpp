9c8dff7a95bb108642165aa9e72a7208846f6175
Andrew <49621025+andrei-voia@users.noreply.github.com>
2019-04-19 02:01:23 +0300

    Code update

---
+++ main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+int n,a,p;
+using namespace std;
+int main()
+{p=0;
+cout<<endl<<endl<<"    Afla cat de mult poti supravietui intr-o confruntare cu CHUCK NORRIS !!!"<<endl<<endl;
+cout<<"     - pentru a incepe testul, tastati 69 (sau orice alt numar) !!!"<<endl<<endl<<endl;
+cout<<" -introduceti numarul aici:  ";
+cin>>n;
+cout<<endl<<endl<<endl<<endl<<endl;
+cout<<"     *  Te plimbi pe strada si il vezi pe CHUCK NORRIS ca se uita urat la tine, ce faci ?"<<endl<<endl;
+cout<<"   1. Te intorci si grabesti pasul."<<endl;
+cout<<"   2. Alergi catre el si incerci sa-l imobilizezi."<<endl;
+cout<<"   3. Te rogi sa vina Sfarsitu' Lumii inaintea lui."<<endl<<endl;
+cout<<"            - alegeti varianta: ";
+cin>>a;
+cout<<endl<<endl<<endl<<endl<<endl<<endl;
+if(a==1){p=p+5; cout<<endl<<endl<<" - Pacat ca CHUCK NORRIS fuge mai repede decat orice pamantean.";}
+if(a==2){p=p+1; cout<<endl<<endl<<" - Te-ai aruncat direct in cusca leului.";}
+if(a==3){p=p+3; cout<<endl<<endl<<" - Mai mari sunt sansele ca UTA sa castige Champions League.";}
+cout<<endl<<endl<<endl<<endl<<endl<<endl;
+
+cout<<"     *  Intr-o confruntare directa cu CHUCK NORRIS, ce arma consideri ca ai     folosi ?"<<endl<<endl;
+cout<<"   1. Macheta lu' Jason din Friday 13th."<<endl;
+cout<<"   2. Un SCAR LIGHT militar automat."<<endl;
+cout<<"   3. Sunt pacifist."<<endl<<endl;
+cout<<"            - alegeti varianta: ";
+cin>>a;
+cout<<endl<<endl<<endl<<endl<<endl<<endl;
+if(a==1){p=p+3; cout<<" - Nu sunt increzator ca poti ajunge asa aproape de CHUCK NORRIS in viata.";}
+if(a==2){p=p+5; cout<<" - Gloantele ricoseaza, asa ca te ciuruiesti singur si nesilit de nimeni.";}
+if(a==3){p=p+1; cout<<" - CHUCK NORRIS nu este.";}
+cout<<endl<<endl<<endl<<endl<<endl<<endl;
+
+cout<<"     *  Ce ai purta sa te portejeze daca CHUCK NORRIS ti-a pus gand rau ?"<<endl<<endl;
+cout<<"   1. Armura lu' IRON MAN din ultimul film."<<endl;
+cout<<"   2. Haine de cocalar, in amestec cu frizura lu' Justin Bieber."<<endl;
+cout<<"   3. Mantia de invizibilitate a lu' Harry Potter."<<endl<<endl;
+cout<<"            - alegeti varianta: ";
+cin>>a;
+cout<<endl<<endl<<endl<<endl<<endl<<endl;
+if(a==1){p=p+5; cout<<" - CHUCK NORRIS la varsta de 2 ani se juca cu metal, nu cu plastelina.";}
+if(a==2){p=p+1; cout<<" - Rau pentru tine, CHUCK NORRIS uraste cocalarii.";}
+if(a==3){p=p+3; cout<<" - Degeaba, CHUCK NORRIS simte frica.";}
+cout<<endl<<endl<<endl<<endl<<endl<<endl;
+
+cout<<"     *  Ce masuri de siguranta ti-ai lua impotriva lui CHUCK NORRIS ?"<<endl<<endl;
+cout<<"   1. Cumpar 2 caini de paza."<<endl;
+cout<<"   2. Imi cumpar o arma de foc si o tin langa mine."<<endl;
+cout<<"   3. Ceri protectie de la Politia Locala."<<endl;
+cout<<"   4. Apelezi la Gelu."<<endl<<endl;
+cout<<"            - alegeti varianta: ";
+cin>>a;
+cout<<endl<<endl<<endl<<endl<<endl<<endl;
+if(a==1){p=p+3; cout<<" - Carne de Shaorma pentru CHUCK NORRIS.";}
+if(a==2){p=p+5; cout<<" - Gloantele ricoseaza, asa ca te ciuruiesti singur si nesilit de nimeni.";}
+if(a==3){p=p+7; cout<<" - Tot ce faci este sa adaugi mai multe cadavre in calcul.";}
+if(a==4){p=p+1; cout<<" - Pana apare Gelu, ai devenit deja una cu pamantul.";}
+cout<<endl<<endl<<endl<<endl<<endl<<endl;
+
+cout<<"     *  Conduci linistit pe sosea, cand, dintr-o data, iti sare in fata masinii CHUCK NORRIS, ce faci ?"<<endl<<endl;
+cout<<"   1. Accelerezi sa dai peste el."<<endl;
+cout<<"   2. Opresti masina si incerci sa-l ajuti."<<endl;
+cout<<"   3. Sari din masina in miscare si incerci sa fugi."<<endl<<endl;
+cout<<"            - alegeti varianta: ";
+cin>>a;
+cout<<endl<<endl<<endl<<endl<<endl<<endl;
+if(a==1){p=p+3; cout<<" - Deformezi masina si pe tine odata cu ea.";}
+if(a==2){p=p+1; cout<<" - Mai bine scrie pe un carton: va rog omorati-ma, si puneti-l pe spate";}
+if(a==3){p=p+5; cout<<" - Poti fugi dar nu te poti ascunde, CHUCK NORRIS te va gasi.";}
+cout<<endl<<endl<<endl<<endl<<endl<<endl;
+
+cout<<"                  *** Felicitari, ai reusit sa supravietuiesti                                     "<<p<<" secunde impotriva lui CHUCK NORRIS ***";
+cout<<endl<<endl<<endl;
+cout<<"                 Esti un ratat total daca ai facut testul acesta,                                   lasa-te de viata si arunca-te pe geam !!!";
+cout<<endl<<endl<<endl<<endl<<endl<<endl;
+return 0;}
